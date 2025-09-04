06ae214116411b989c739d2d0f09729a52ab0a66
juan <nobert407@gmail.com>
2025-09-04 11:14:49 -0300

    Elmentos criados

--- classes/expense.js
+++ classes/expense.js
@@ -1,6 +1,7 @@
 export default  class Expense {
-    constructor(categoria,preco){
+    constructor(categoria,preco,nome){
         this.id = new Date().getDate(),
+        this.nome = nome.value
         this.category_id = categoria.value
         this.categoria_name = categoria.options[categoria.selectedIndex].text,
         this.amount = preco.value
--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const input = document.querySelector("#amount")
 const nome = document.querySelector("#expense")
 const categoria = document.querySelector("#category")
 const form = document.querySelector("form")
+const lista = document.querySelector('ul')
 
 
 input.addEventListener("input" ,() => {
@@ -25,7 +26,48 @@ function formatarValor(valor){
 form.onsubmit = (e) =>{
     e.preventDefault()
     console.log("sub")
-    let despesa = new Expense(categoria,input)
+    let despesa = new Expense(categoria,input,nome)
     console.log(despesa)
+    adicionarDespesa(despesa)
+
+}
+
+function adicionarDespesa(despesa){
+    const li = document.createElement("li")
+    li.classList.add('expense')
+    const img = document.createElement("img")
+    //Imagem
+    img.setAttribute('src', `./img/${despesa['category_id']}.svg`)
+   
+    //Nomes
+    const div = document.createElement("div")
+    div.classList.add('expense-info')
+    const nome = document.createElement('strong')
+    nome.innerHTML = despesa['nome']
+    const tipo = document.createElement('span')
+    tipo.innerHTML = despesa.categoria_name
+    div.append(nome,tipo)
+// <span class="expense-amount"><small>R$</small>1.420,57</span>
+
+//<img src="./img/remove.svg" alt="remover" class="remove-icon" />
+
+    //preco
+    const span = document.createElement('span')
+    span.classList.add('expense-amount')
+    const small = document.createElement('small')
+    small.innerText = 'R$'
+    span.innerHTML = despesa.amount
+    span.prepend(small)
+
+    //Excluir
+     const imgRemove = document.createElement("img")
+    //Imagem
+    imgRemove.classList.add('remove-icon')
+    imgRemove.setAttribute('src','./img/remove.svg')
+
+
+    li.append(img,div,span,imgRemove)
+
+    lista.append(li)
 
 }
