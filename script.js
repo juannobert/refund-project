b330c7726db7a9fcd22ddc693ef86fc2a7b95a37
juan <nobert407@gmail.com>
2025-09-04 10:17:34 -0300

    Formatando moeda

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 const input = document.querySelector("#amount")
-input.addEventListener("input" ,() => {
-    let value = input.value.replace(/\D/g,"") // tira as letras
-    input.value = value
 
-    
+
+
+input.addEventListener("input" ,() => {
+    let valor = input.value.replace(/\D/g,"") // tira as letras
+    valor = Number(valor) / 100 //Tranforma em centavos
+    input.value = formatarValor(valor)    
 })
+
+//Formata no padrão BRL
+function formatarValor(valor){
+    valor = valor.toLocaleString("pt-BR",{
+        style: "currency",
+        currency: "BRL"
+    })
+    return valor
+
+}
